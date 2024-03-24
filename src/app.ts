628ad8330136426881f02997a414d676782efb07
Filoji <michelvespier@gmail.com>
2024-03-24 11:33:18 +0100

    Added Nodemon and CORS

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from "express";
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 3000;
+app
+  .use(cors({
+  	origin: "*"
+}))
 
 app.get('/', (req : Request, res : Response) => {
 	res.send({
